cc459457a3f3d74a08a8609fd30b5a6273f25bc4
yammerk <35222567+yammerk@users.noreply.github.com>
2018-01-11 17:03:50 +0100

    Update main.py
    added code that includes names of cities

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ f3 = open('MokumAirwaysCities.txt')
 passengermatrix = open('MokumAirwaysPassengers.txt').read()
 passengermatrix = [item.split(',') for item in passengermatrix.split('\n')[:-1]]
 
-
-
 distancematrix = open('MokumAirwaysDistances.txt').read()
 distancematrix = [item.split(',') for item in distancematrix.split('\n')[:-1]]
+
+citiesmatrix = open('MokumAirwaysCities.txt').read()
+splitcitiesmatrix = [citiesmatrix.split(',') for citiesmatrix in citiesmatrix.split('\n')[:-1]]
+
 
 pkresult = [[0 for x in range(28)] for y in range(28)]
 
@@ -18,7 +20,6 @@ for i in range(len(passengermatrix)):
    for j in range(len(distancematrix[0])):
           pkresult[i][j] += int(passengermatrix[i][j]) * int(distancematrix[i][j])
 
-print pkresult
 
 tresult = [[0 for x in range(28)] for y in range(28)]
 
@@ -26,13 +27,8 @@ for i in range(len(distancematrix)):
     for j in range(len(distancematrix)):
         tresult[i][j] = int(distancematrix[i][j])/800.0
 
-print tresult
+for i in range(len(pkresult[0])):
+    value = max(pkresult[i])
+    valueandpos = pkresult[i].index(value)
 
-###result = [[sum(a*b for a,b in zip(passengermatrix_row, distancematrix_col)) for distancematrix_col in zip(*distancematrix)] for passengermatrix_row in passengermatrix]
-###for r in result:
-###   print(r)
-
-
-
-#for r in result:
-    #print(r)
+print splitcitiesmatrix[3][3]
